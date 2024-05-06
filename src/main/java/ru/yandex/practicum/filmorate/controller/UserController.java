ecebddb34ed14dde1bdb1bdea42cded0569f7d5b
kafi <k.f.i@ya.ru>
2024-05-06 21:06:33 +0300

    Добавить модель User Добавить модель Film Добавить контроллер UserController Добавить контроллер FilmController Добавить валидатор минимальной даты Создать аннотацию для минимальной даты Добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,7 +5,6 @@ import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
