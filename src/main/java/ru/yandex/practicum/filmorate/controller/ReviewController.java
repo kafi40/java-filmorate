0ae3edd9f777c20b98527948c21891f1ea78a389
fikalinichev <k.f.i@ya.ru>
2024-09-04 18:05:54 +0300

    Init Commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+}
